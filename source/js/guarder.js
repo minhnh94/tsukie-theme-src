987568d3584db8f17173050294c42ce793cebcdb
minhnh <minhnh.da@gmail.com>
2019-04-11 17:24:08 +0900

    change adblock content

--- source/js/guarder.js
+++ source/js/guarder.js
@@ -13,8 +13,8 @@ if (window.canRunAds === undefined) {
     -webkit-box-shadow:0 0 10px rgba(0,0,0,0.4);
     -moz-box-shadow:0 0 10px rgba(0,0,0,0.4); 
     box-shadow:0 0 10px rgba(0,0,0,0.4);'>
-				<h2>We only serve a handful of minimal, non-intrusive ads. No pop-ups, sounds, movies... ever! Please disable Adblock or whitelist our website to motivate us! After disabling Adblock or whitelisting our website, please refresh the page to reload the content.</h2><br>
-				<h2>Website chỉ có rất ít quảng cáo, và đó là những banner quảng cáo siêu nhỏ gọn, không có popup, video, tiếng, v...v. Xin hãy tắt Adblock để ủng hộ bọn mình! Sau khi tắt Adblock, hãy load lại trang nhé.</h2><br>
+				<h2>We only serve a handful of minimal, non-intrusive ads that will not hinder your reading experience. No pop-ups, sounds, movies... ever! Please disable Adblock or whitelist our website to motivate us! After disabling Adblock or whitelisting our website, please refresh the page to reload the content.</h2><br>
+				<h2>Website chỉ có rất ít quảng cáo, và đó là những banner quảng cáo siêu nhỏ gọn không ảnh hưởng đến trải nghiệm đọc, không có popup, video, tiếng, v...v. Xin hãy tắt Adblock để ủng hộ bọn mình! Sau khi tắt Adblock, hãy load lại trang nhé.</h2><br>
 				<h2>このサイトには僅かにとても小さく、迷惑とならない広告バナーのみが表示されます。ポップアップ、音声再生、動画再生。。一切ありません。Adblockを無効にしていただけますと大変ありがたいです！お願いします！Adblockを無効にしていただいた後、ページを再読み込み、リロードしていただくとサイトが正常に動作します。</h3>
 			</div>
 		</div>
