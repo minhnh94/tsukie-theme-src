bbb1d8cf23e99c943398c4ff1da35e1bc95c9689
minhnh <minhnh.da@gmail.com>
2019-05-24 01:50:11 +0900

    disable some anti adblock code

--- source/js/guarder2.js
+++ source/js/guarder2.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
             $('img').attr("src", "https://puu.sh/Do8z9.png");
             $('a.gallery-item').attr('href', '');
             $('.thumbnail-image').attr("style", "background-image:url(https://i.imgur.com/pRZNTzd.jpg)");
-            $('.line').text('We only serve a handful of minimal, non-intrusive ads that will not hinder your reading experience. No pop-ups, sounds, movies... ever! Please disable Adblock or whitelist our website to motivate us! After disabling Adblock or whitelisting our website, please refresh the page to reload the content.');
-            $('.article-entry a').not(":has(img)").text('Please disable Adblock to see the link').attr('href', '');
+            // $('.line').text('We only serve a handful of minimal, non-intrusive ads that will not hinder your reading experience. No pop-ups, sounds, movies... ever! Please disable Adblock or whitelist our website to motivate us! After disabling Adblock or whitelisting our website, please refresh the page to reload the content.');
+            // $('.article-entry a').not(":has(img)").text('Please disable Adblock to see the link').attr('href', '');
             $('body').append(`
 		<div class='guarder'>
 			<div class='guarder-modal' style='
